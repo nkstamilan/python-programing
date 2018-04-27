062014ccac311c484631ca34d6498ae5e22df95a
nkstamilan <37336232+nkstamilan@users.noreply.github.com>
2018-04-27 11:22:50 +0530

    Update armstrong.py

--- beginner level/set2/armstrong.py
+++ beginner level/set2/armstrong.py
@@ -1,9 +1,14 @@
-sum=0
+s=0
 a=[]
 a=input()
 b=len(a)
 for i in range(b):
   c=a[i]
   d=pow(int(c),b)
-  sum=sum+d
-print(sum)
+  s=s+d
+print(s)
+
+if(a==str(s)):
+  print('armstrong number')
+else:
+  print('not an armstrong number')
