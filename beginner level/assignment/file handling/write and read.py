dcc7c678128e368cfeeb1bef9b762b473c1fa660
nkstamilan <37336232+nkstamilan@users.noreply.github.com>
2018-04-30 11:52:04 +0530

    Update write and read.py

--- beginner level/assignment/file handling/write and read.py
+++ beginner level/assignment/file handling/write and read.py
@@ -1,5 +1,5 @@
 def main():
   fhandle= open("file location or file name"+"w+)  '''w+ for write mode of a file'''
-  fhandle.write("something")  
+  fhandle.write("something")  '''fhandle.writelines("something") for writing 5 lines and readlines for reading 5 lines''' 
   fhandle.seek(0)         '''to rewind the function at the begining'''
   print(fhandle.read())   '''if this is given after line 3 it wont read because it acts as tape recorder'''
