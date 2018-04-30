fca7109c813d7916c5cc8cea08e1cd8bb96b3343
nkstamilan <37336232+nkstamilan@users.noreply.github.com>
2018-04-30 09:40:38 +0530

    Update end.py

--- beginner level/print/end.py
+++ beginner level/print/end.py
@@ -1,3 +1,8 @@
 #python 3
 print("GeeksforGeeks",end=' ')
-print("Python" , end = ' ')     #print("Python")-same output
+print("Python" , end = ' ')     
+#By default python’s print() function ends with a newline. 
+#A programmer with C/C++ background may wonder how to print without newline.
+#Python’s print() function comes with a parameter called ‘end’. 
+#By default, the value of this parameter is ‘\n’, i.e. the new line character.
+#You can end a print statement with any character/string using this parameter.
